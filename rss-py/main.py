d7bd67734cd7b2991ed8e54a8f3c5adc1ca710b3
Mahdi <m.mahdisaeedi99@gmail.com>
2020-05-05 14:59:22 -0400

    Add redis for save data

--- rss-py/main.py
+++ rss-py/main.py
@@ -1,4 +1,4 @@
-#import redis
+import redis
 import feedparser
 import os
 import json
@@ -9,14 +9,16 @@ url = 'http://farsi.khamenei.ir/rss'
 response = feedparser.parse(url)
 directory = "/var/log/py-report/"
 os.system("mkdir " + directory)
+redisclient = redis.Redis()
 
 for item in response.entries:
-    year = "%d" % item.published_parsed.tm_year
-    month = "%02d" % item.published_parsed.tm_mon
-    day = "%02d" % item.published_parsed.tm_mday
-    filedirectory = directory + year + "/" + month + "/" + day + "/"
-    os.system("mkdir -p " + filedirectory)
     guid = re.findall("/(\d*)$", item.guid)[0]
-    jsonfile = open(filedirectory + guid + ".json", "w")
-    jsonfile.write(json.dumps(item))
-    jsonfile.close()
+    if(redisclient.sadd("items", guid)):
+        year = "%d" % item.published_parsed.tm_year
+        month = "%02d" % item.published_parsed.tm_mon
+        day = "%02d" % item.published_parsed.tm_mday
+        filedirectory = directory + year + "/" + month + "/" + day + "/"
+        os.system("mkdir -p " + filedirectory)
+        jsonfile = open(filedirectory + guid + ".json", "w")
+        jsonfile.write(json.dumps(item))
+        jsonfile.close()
