0f0bda9a6ac73ed05f9478367f927ba3546c0bfb
Mahdi <m.mahdisaeedi99@gmail.com>
2020-05-05 16:00:05 -0400

    Feature: send email for new news

---
+++ rss-py/agents.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class EmailAgent:
+
+    def __init__(self, host, port, username, password):
+        self.host = host
+        self.port = port
+        self.smtp = smtplib.SMTP(host, port)
+        self.smtp.starttls()
+        self.smtp.login(username, password)
+
+    def send(self, from_, to, subject, body):
+        # Create a message
+        message = MIMEMultipart()
+
+        # Setup the parameters of the message
+        message['From'] = from_
+        message['To'] = to
+        message['Subject'] = subject
+
+        # Add in the message body
+        message.attach(MIMEText(body, 'plain'))
+
+        # Sendding the message
+        self.smtp.send_message(message)
+        self.smtp.quit()
+
--- rss-py/main.py
+++ rss-py/main.py
@@ -3,22 +3,44 @@ import feedparser
 import os
 import json
 import re
+import .EmailAgent
 
 
-url = 'http://farsi.khamenei.ir/rss'
-response = feedparser.parse(url)
-directory = "/var/log/py-report/"
-os.system("mkdir " + directory)
-redisclient = redis.Redis()
+def main():
+    url = 'http://farsi.khamenei.ir/rss'
+    response = feedparser.parse(url)
+    directory = "/var/log/py-report/"
+    os.system("mkdir " + directory)
+    redisclient = redis.Redis()
 
-for item in response.entries:
-    guid = re.findall("/(\d*)$", item.guid)[0]
-    if(redisclient.sadd("items", guid)):
-        year = "%d" % item.published_parsed.tm_year
-        month = "%02d" % item.published_parsed.tm_mon
-        day = "%02d" % item.published_parsed.tm_mday
-        filedirectory = directory + year + "/" + month + "/" + day + "/"
-        os.system("mkdir -p " + filedirectory)
-        jsonfile = open(filedirectory + guid + ".json", "w")
-        jsonfile.write(json.dumps(item))
-        jsonfile.close()
+    for item in response.entries:
+        guid = re.findall("/(\d*)$", item.guid)[0]
+        if(redisclient.sadd("items", guid)):
+            year = "%d" % item.published_parsed.tm_year
+            month = "%02d" % item.published_parsed.tm_mon
+            day = "%02d" % item.published_parsed.tm_mday
+            filedirectory = directory + year + "/" + month + "/" + day + "/"
+            os.system("mkdir -p " + filedirectory)
+            jsonfile = open(filedirectory + guid + ".json", "w")
+            jsonfile.write(json.dumps(item))
+            jsonfile.close()
+            sendnew(item)
+
+def sendnew(self, item):
+    with open("configfile.txt", "r") as configfile:
+        host = configfile.readline()
+        port = configfile.readline()
+        username = configfile.readline()
+        password = configfile.readline()
+        destination = configfile.readline()
+        configfile.close()
+        agent = EmailAgent(host, port, username, password)
+
+        subject = 'New news is available'
+        body =
+            'title ' + item.title + '\n'
+            + 'link' + item.link + '\n'
+            + 'description' + item.description
+
+        agent.send(username, destination, subject, body)
+
